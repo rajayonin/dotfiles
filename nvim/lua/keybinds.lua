ddc3aa278f111847b633914fc8e3829645317886
L. Daniel Casais <71148501+rajayonin@users.noreply.github.com>
2024-02-29 12:19:18 +0100

    Update keybinds.lua
    jk

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -2,3 +2,5 @@
 vim.g.mapleader = ' '
 -- easy escape from terminal
 vim.keymap.set('t', "<Esc><Esc>", "<C-\><C-n>", "noremap")
+-- jk to Esc (watch out Dijkstra)
+vim.keymap.set("i", "jk", "<Esc>", "noremap")

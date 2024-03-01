f82d9202799b656f6db267e077a2cad4d1745a3f
ldcas-uc3m <100429021@alumnos.uc3m.es>
2024-03-01 16:54:45 +0100

    plugins

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,1 +1,1 @@
-vim.cmd([[colorscheme pablo]])
+vim.cmd([[colorscheme default]])
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -22,6 +22,23 @@ vim.opt.smartcase = true
 -- highlight current line
 -- vim.opt.cursorline = true
 
+-- cursor blink
+vim.o.guicursor = table.concat({
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+}, ",")
+
+-- whitespace
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- preview substitutions live
+vim.opt.inccommand = 'split'
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
 -- set tabs
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
@@ -30,11 +47,15 @@ vim.opt.tabstop = 4
 
 -- set wrap
 vim.opt.wrap = true
+vim.opt.breakindent = true
 
 -- save backups and other trash to another place so you don't see them
 HOME = os.getenv("HOME")
 vim.opt.backupdir = HOME .. "/.local/share/nvim/backup"
 vim.opt.directory = HOME .. "/.local/share/nvim/swap"
+
+-- save undo history
+vim.opt.undofile = true
 
 -- Ignore files with these extensions:
 vim.opt.wildignore = "*.odt,*.doc*,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.JPG,*.exe,*.bmp,*.flv,*.gz,*.tgz,*.zip,*.iso,*.gzip,*.mov,*.xz,*.tar,*.img,*.docx,*.xlsx,*.xls"

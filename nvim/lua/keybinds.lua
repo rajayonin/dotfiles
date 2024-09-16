0e908b99d75e16dffd26054cf0c5aa90c20fcc70
ldcas-uc3m <100429021@alumnos.uc3m.es>
2024-09-16 15:46:20 +0200

    kj rules

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -4,10 +4,13 @@ vim.g.mapleader = ' '
 -- easy escape from terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- jk to Esc (watch out Dijkstra)
-vim.keymap.set("i", "jk", "<Esc>", {noremap = true})
+-- kj to Esc
+vim.keymap.set("i", "kj", "<Esc>", {noremap = true})
 
 -- prevent x from copying
 vim.keymap.set("n", "x", '"_x', {noremap = true})
 vim.keymap.set("n", "X", '"_X', {noremap = true})
 
+-- prevent errors when exiting
+vim.api.nvim_create_user_command("X", "x", {})
+vim.api.nvim_create_user_command("Q", "q", {})

d2a4c017c23b7443c1fbce880ca0c177cefd9751
L. Daniel Casais <71148501+rajayonin@users.noreply.github.com>
2024-10-16 09:25:12 +0200

    Update keybinds.lua
    L/H to end/begin line

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -10,6 +10,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- kj to Esc
 vim.keymap.set("i", "kj", "<Esc>", {noremap = true})
 
+-- L/H to end/begin line
+vim.keymap.set({"n", "v"}, "L", "$", {noremap = true})
+vim.keymap.set({"n", "v"}, "H", "0", {noremap = true})
+
 -- prevent x/d from copying
 vim.keymap.set("n", "x", '"_x', {noremap = true})
 vim.keymap.set("n", "X", '"_X', {noremap = true})

4af8e7a02ce3012f4fd2449fa62c4739892998a6
rajayonin <71148501+rajayonin@users.noreply.github.com>
2025-08-17 18:35:23 +0200

    [nvim] more formatters, small cleanup

--- nvim/.config/nvim/lua/lsp/mason-lspconfig.nvim.lua
+++ nvim/.config/nvim/lua/lsp/mason-lspconfig.nvim.lua
@@ -1,38 +1,38 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
+  "mason-org/mason-lspconfig.nvim",
 
-	enabled = true,
+  enabled = true,
 
-	dependencies = {
-		{
-			"mason-org/mason.nvim",
-			opts = {
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			},
-		},
-		"neovim/nvim-lspconfig",
-	},
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      },
+    },
+    "neovim/nvim-lspconfig",
+  },
 
-	opts = {
-		ensure_installed = {
-			-- LSPs
-			"lua_ls",
-			"jsonls",
-			"yamlls",
-			"bashls",
-			"dockerls",
-			"ltex", -- Markdown, LaTeX, etc.
-			"clangd", -- C/C++
-			"rust_analyzer", -- 🦀
-			"ruff", -- Python
-			"tinymist", -- Typst
-			"vimls",
-		},
-	},
+  opts = {
+    ensure_installed = {
+      -- LSPs
+      "lua_ls",
+      "jsonls",
+      "yamlls",
+      "bashls",
+      "dockerls",
+      "ltex", -- Markdown, LaTeX, etc.
+      "clangd", -- C/C++
+      "rust_analyzer", -- 🦀
+      "ruff", -- Python
+      "tinymist", -- Typst
+      "vimls",
+    },
+  },
 }
--- nvim/.config/nvim/lua/lsp/mason-tool-installer.nvim.lua
+++ nvim/.config/nvim/lua/lsp/mason-tool-installer.nvim.lua
@@ -1,20 +1,23 @@
 return {
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
 
   enabled = true,
 
-	opts = {
-		ensure_installed = {
-			-- linters
-			"pylint",
-			"eslint_d",
-			"ast-grep",
-			-- formatters
-			"prettier",
-			"stylua",
-		},
-		integrations = {
-			["mason-lspconfig"] = true,
-		},
-	},
+  opts = {
+    ensure_installed = {
+      -- linters
+      "pylint",
+      "eslint_d",
+      "ast-grep",
+      -- formatters
+      "prettier",
+      "stylua",
+      "typstyle",
+      "jq",
+      "clang-format",
+    },
+    integrations = {
+      ["mason-lspconfig"] = true,
+    },
+  },
 }
